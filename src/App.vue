643a6a6e52f58049fb3955f241271834e02bfb26
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-29 11:19:48 +0100

    add current weight

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,89 @@
-<script setup></script>
+<script setup>
+import { ref, shallowRef, computed, watch, nextTick } from "vue";
+import Chart from "chart.js/auto";
+
+const weights = ref([]);
+const weightChartEl = ref(null);
+const weightChart = shallowRef(null);
+const weightInput = ref(0);
+const currentWeight = computed(() => {
+  return weights.value.sort((a, b) => b.date - a.date)[0] || { weight: 0 };
+});
+
+const addWeight = () => {
+  weights.value.push({
+    weight: weightInput.value,
+    date: new Date().getTime(),
+  });
+};
+</script>
 
 <template>
-  <h1>Hello, Vue</h1>
+  <main>
+    <h1>Weight Tracker</h1>
+
+    <div class="current">
+      <span>{{ currentWeight.weight }}</span>
+      <small>Current Weight (kg)</small>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "montserrat", sans-serif;
+}
+
+body {
+  background-color: #eee;
+}
+
+main {
+  padding: 1.5rem;
+}
+
+h1 {
+  font-size: 2em;
+  text-align: center;
+  margin-bottom: 2rem;
+}
+
+h2 {
+  margin-bottom: 1rem;
+  color: #888;
+  font-weight: 400;
+}
+
+.current {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 200px;
+  height: 200px;
+
+  text-align: center;
+  background-color: white;
+  border-radius: 999px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  border: 5px solid hwb(330 41% 0%);
+
+  margin: 0 auto 2rem;
+}
+
+.current span {
+  display: block;
+  font-size: 2em;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+.current small {
+  color: #888;
+  font-style: italic;
+}
+</style>
